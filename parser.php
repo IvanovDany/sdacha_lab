138d791354277a22d0b37f9264f65ae07eb1da0d
Baranov Pavel <barash.11999@gmail.com>
2020-04-21 13:15:27 +0500

    Парсер

--- parser.php
+++ parser.php
@@ -65,3 +65,62 @@ if ($data['code'] == 200){
 } else {
   die('Что-то пошло не так');
 }
+
+function curlGetContents($pageUrl, $baseUrl,  $retry = true) {
+  $errors = [];
+  //Получаем дескриптор cURL;
+  $ch = curl_init();
+  //Установка параметров для имеющегося сеанса/дескриптора
+  curl_setopt($ch, CURLOPT_HEADER, false); // отключить вывод заголовков
+  curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false); //для остановки cURL от проверки сертификата узла сети.
+  curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0); // не проверять существование имени общего имени в сертификате ssl
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //для возврата результата передачи в качестве строки из curl_exec() вместо прямого вывода в браузер.
+  curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // Содержимое заголовка "User-Agent: ", посылаемого в HTTP-запросе.
+
+  curl_setopt($ch, CURLOPT_URL, $pageUrl); // Загружаемый URL.
+  curl_setopt($ch, CURLOPT_REFERER, $baseUrl); // Содержимое заголовка "Referer: ", который будет использован в HTTP-запросе.
+
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // для следования любому заголовку "Location: ", отправленному сервером в своем ответе
+
+  $response['data'] = curl_exec($ch);//Выполняет запрос cURL
+
+  $ci = curl_getinfo($ch);//Чекаем инфу ради получения статуса ответа http_code
+  
+  if($ci['http_code'] != 200 && $ci['http_code'] != 404) {
+    $errors[] = [1, $pageUrl, $ci['http_code']];
+    //попытка повторного подключения, если статусы не 200 и не 404
+    if($retry) {
+      sleep(4);
+      $response['data'] = curl_exec($ch);
+      $ci = curl_getinfo($ch);
+      //если ситуация не поменялась, то пишем ошибку
+      if($ci['http_code'] != 200 && $ci['http_code'] != 404){
+        $errors[] = [2, $pageUrl, $ci['http_code']];
+      }
+    }
+  }
+
+  $response['code'] = $ci['http_code'];
+  $response['errors'] = $errors;
+
+  curl_close($ch);//Завершает сеанс cURL
+
+  return $response;
+}
+
+
+function parseContent(DOMXpath $xPath, $query = '//',$is_price=false)
+{
+  $result = [];
+  $q = $xPath->query($query);
+  foreach ($q as $k => $item) {
+    $textRaw = trim($item->textContent);
+    if($is_price){
+    $textRaw = trim($textRaw, '(Avg Bid)');
+    $result[] = trim($textRaw);
+    }
+    else
+      $result[] = $textRaw;
+  }
+  return $result;
+}
