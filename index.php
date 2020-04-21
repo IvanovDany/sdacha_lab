8578028c6ad1f0afef125242c93c56a601cae5be
Baranov Pavel <barash.11999@gmail.com>
2020-04-21 12:32:51 +0500

    Верстка главной страницы

---
+++ index.php
@@ -0,0 +1,47 @@
+<?
+require 'include/connection.php';
+?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset='UTF-8'>
+	<title>Pink Panther</title>
+	<link rel='stylesheet' href='assets/css/style.css'>
+	<link rel='stylesheet' href='libs/css/bootstrap.css'>
+	<link rel="stylesheet" href="libs/css/font-awesome.css">
+
+</head>
+	<body>
+
+<?
+if(isset($_SESSION['logged_user'])){
+?>
+	<div class="header">
+		<p>Привет, <?=$_SESSION['logged_user']['login'];?>.</p>
+		<a href='logout.php'>Выйти</a>
+	</div>
+
+	<button class='show-add-Task'>Создать задачу</button>
+	<form id='add-form' action='create.php' method='POST'>
+		<p>
+			<p>
+				<strong>Название задачи</strong>
+			</p>
+			<input class='task-Name' type='text' name='task_name'>
+		</p>
+		<p>
+			<p>
+				<strong>Описание</strong>
+			</p>	
+			<textarea class='task-Description' name='task_description' rows='5' cols='50'></textarea>
+		</p>
+		<p>
+			<button class='add-Task' type='submit' name='do_create'>Создать</button>
+		</p>
+	</form>
+	<div class ='users-tasks'>
+		<div class ='items'>
+			
+		</div>
+	</div>
+<?}?>
